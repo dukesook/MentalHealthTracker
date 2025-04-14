423642ab82235a11342fd22d0eff5ceccf9eca44
dukesook <devonsookhoo14@gmail.com>
2025-04-14 17:23:52 -0600

    initial style.css

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ const PORT = 3000;
 
 app.set("view engine", "ejs"); // Use EJS as the template engine
 app.use(express.json());
+app.use(express.static('public'));
 
 
 app.get("/", function(req, res) {
-  let loggedIn = true;
+  let loggedIn = true; // TODO - Somehow check if user is logged in
 
   if (loggedIn) {
     res.render("pages/index");
@@ -27,7 +28,6 @@ app.get("/evaluation", function(req, res) {
 
 
 app.get("/checkin", function(req, res) {
-  console.log("checkin")
   res.render("pages/checkin");
 })
 
@@ -40,6 +40,7 @@ app.get("/tracker", function(req, res) {
 app.get("/settings", function(req, res) {
   res.render("pages/settings");
 })
+
 
 app.get("/login", function(req, res) {
   res.render("pages/login");
