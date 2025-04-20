ef29e68960595438fc1ca0e5005e3261a148981a
dukesook <devonsookhoo14@gmailcom>
2025-04-20 10:06:06 -0600

    Resolve git merge conflict in user.mjs

--- models/user.mjs
+++ models/user.mjs
@@ -14,13 +14,10 @@ userSchema.methods.listAll = function(){
   return this.model('User').find({})
 }
 
-<<<<<<< HEAD
-=======
 userSchema.methods.get_first_name = function(){
   return this.model('User').first_name;
 }
 
->>>>>>> 104392a1b25a1c0f41d733cc4368c05dd6d13931
 const modelName = 'User';
 const userModel = mongoose.model(modelName, userSchema);
 
