3fc7518ff0dbc90d19515f42bf37f0f8d6e6f337
dukesook <devonsookhoo14@gmail.com>
2025-04-28 11:00:52 -0600

    add expect to 'createDailyCheckin()' test

--- test/database.test.mjs
+++ test/database.test.mjs
@@ -26,51 +26,17 @@ test('collectionNames[]', () => {
   expect(Database.collectionNames).toContain('users');
 });
 
-// test('getCollection()', async() => {
-//   const result = await Database.getCollection('dailycheckins');
-//   expect(Array.isArray(result)).toBe(true);
-// }) 
-
-// describe('getCollection()', () => {
-//   it('should query all documents when no userId is provided', async () => {
-
-//     // Spy on helper function
-//     const spy_getModel = vi.spyOn(Database, 'getModel');
-
-//     // Define a mock object to simulate the dailyCheckinModel
-//     const mock_dailyCheckinModel = {
-//       find: async function() {
-//         const checkins = [
-//           { check_in_date: '2023-10-01', mood: 'happy', journal: 'Had a great day!' },
-//           { check_in_date: '2023-10-02', mood: 'sad', journal: 'Not so good today.' },
-//           { check_in_date: '2023-10-03', mood: 'neutral', journal: 'Just another day.' }
-//         ];
-//         return checkins;
-//       }
-//     }
-//     spy_getModel.mockReturnValue(mock_dailyCheckinModel);
-
-//     const collection = await Database.getCollection('dailycheckins');
-//     expect(collection).toBeDefined();
-//   })
-// })
-
 test('clear_database()', async() => {
   const spy = vi.spyOn(Database, 'clear_database')
 
 });
 
 function getMock_dailyCheckin() {
-  function mock_dailyCheckin() {
-    return {
+  return {
+    default: vi.fn(() => ({
       save: vi.fn().mockResolvedValue()
-    }
-  }
-
-  const mock_dailyCheckinModel = {
-    default: mock_dailyCheckin
-  }
-  return mock_dailyCheckinModel;
+    }))
+  };
 }
 
 
@@ -81,11 +47,20 @@ describe('createDailyCheckin()', async() => {
 
     const fakeUserId = new mongoose.Types.ObjectId();
     await Database.createDailyCheckin(fakeUserId, new Date(), 'mood', 'journal');
+    expect(dailyCheckinModel).toHaveBeenCalledWith(
+    {
+      user_id: fakeUserId,
+      check_in_date: expect.any(Date),
+      mood: 'mood',
+      journal: 'journal'
+    });
   });
 });
 
 /*
 vi.mock()
+  - "When you import this module, replace it with this fake version."
   - Argument #1 - The path to the module to be mocked
   - Argument #2 - A Function that returns an object to replace the module
+  - Returns - 
 */
