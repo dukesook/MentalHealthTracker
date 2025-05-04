586dcb5390dedc1cbb1a1fa9ad8bff391135ccf2
Edgar Ochoa <eochoas@nmsu.edu>
2025-05-03 19:11:04 -0600

    fix dark mode for take_a_test.ejs

--- public/script.js
+++ public/script.js
@@ -3,8 +3,17 @@ function darkmode() {
     localStorage.setItem('darkmode', !wasDarkmode);
     const element = document.body;
     element.classList.toggle('dark-mode', !wasDarkmode);
+    const labels = document.querySelectorAll('label');
+    labels.forEach(label=>{
+        label.classList.toggle('dark-mode', !wasDarkmode);
+    });
+   
 }
 
 function onload() {
     document.body.classList.toggle('dark-mode', localStorage.getItem('darkmode') === 'true');
+    const labels = document.querySelectorAll('label');
+    labels.forEach(label=>{
+        label.classList.toggle('dark-mode',localStorage.getItem('darkmode') === 'true');
+    });
 }
