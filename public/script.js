a01e3c1cd0f08e62c1756559f4f0023cc976a2fb
Edgar Ochoa <eochoas@nmsu.edu>
2025-05-06 18:59:52 -0600

    comment script.js

--- public/script.js
+++ public/script.js
@@ -1,7 +1,8 @@
 function darkmode() {
     const wasDarkmode = localStorage.getItem('darkmode') === 'true';
     localStorage.setItem('darkmode', !wasDarkmode);
-
+    // The labels in the test section do not change like the rest of the
+    // text does in darkmode, thus it must be done per label as below 
     const isDark = !wasDarkmode;
     document.body.classList.toggle('dark-mode', isDark);
     document.querySelectorAll('label').forEach(label => {
